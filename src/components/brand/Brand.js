a85b8d0c6370c05b668bf36f26ea61957daa7f29
HosseinGhiasi <hosseinghiasi5727@gmail.com>
2022-01-24 17:06:45 +0330

    optimized header image

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { brandImages } from './imports'
 import './brand.css'
 
 function Brand() {
     return (
-        <div>
-            Brand
+        <div className='gpt3__brand section__padding'>
+            {brandImages.map(item => (
+                <div key={item.id}>
+                    <img
+                        src={item.img}
+                        alt={item.img.toString()}
+                    />
+                </div>
+            ))}
         </div>
     )
 }
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export const brandImages = [
+    { id: 1, img: google },
+    { id: 2, img: slack },
+    { id: 3, img: atlassian },
+    { id: 4, img: dropbox },
+    { id: 5, img: shopify },
+]
